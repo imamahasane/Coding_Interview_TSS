5c7521b9068641bff12eccf0dc10d84f477953fb
Md Imam Ahasan <emamahasane@gmail.com>
2022-03-20 00:24:21 +0600

    Add files via upload

---
+++ Coding Interview TSS/3.1 duplicates from array.py
@@ -0,0 +1,11 @@
+
+def remove_duplicates(nums):
+    unique_nums = []
+    unique_nums.append(nums[0])
+    
+    n = len(nums)
+    for i in range(1, n):
+        if nums[i] != nums[i - 1]:
+            unique_nums.append(nums[i])
+            
+    return len(unique_nums)
